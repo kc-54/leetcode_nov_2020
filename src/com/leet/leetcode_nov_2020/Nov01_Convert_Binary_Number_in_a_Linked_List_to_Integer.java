c12d3649ea185f83c5b041ad21aab5193513c60b
stupidDummy <kelvinchandra54@yahoo.com>
2020-11-02 14:52:26 +0800

    init value

---
+++ src/com/leet/leetcode_nov_2020/Nov01_Convert_Binary_Number_in_a_Linked_List_to_Integer.java
@@ -0,0 +1,44 @@
+package com.leet.leetcode_nov_2020;
+
+public class Nov01_Convert_Binary_Number_in_a_Linked_List_to_Integer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ListNode head = new ListNode(1);
+		head.next = new ListNode(0);
+		
+		System.out.println(getDecimalValue(head));
+	}
+
+	public static int getDecimalValue(ListNode head) {
+        StringBuilder sb = new StringBuilder();
+        
+        sb.append(head.val);
+        
+        ListNode node = head;
+        
+        while(node.next!=null){
+            node = node.next;
+            sb.append(node.val);
+        }
+        
+        return Integer.parseInt(sb.toString(),2);  
+    }
+	
+	public static class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode() {
+		}
+
+		ListNode(int val) {
+			this.val = val;
+		}
+
+		ListNode(int val, ListNode next) {
+			this.val = val;
+			this.next = next;
+		}
+	}
+}
